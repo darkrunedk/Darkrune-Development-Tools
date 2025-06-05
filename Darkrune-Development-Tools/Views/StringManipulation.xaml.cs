e39288e519f8fa61d5430707bc2a9b67cec89b33
Tobias Belling Christensen <github@darkrune.dk>
2025-06-05 20:29:35 +0200

    Added some information about each point in Home. Added a new page for string manipulation. Updated some styling that can be used program-wide.

---
+++ Darkrune-Development-Tools/Views/StringManipulation.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Darkrune_Development_Tools.Views
+{
+    /// <summary>
+    /// Interaction logic for StringManipulation.xaml
+    /// </summary>
+    public partial class StringManipulation : Page
+    {
+        public StringManipulation()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteActionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string toText = FromTxtBox.Text;
+            string seperator = SeperatorSeletor.Text;
+            if (seperator.Equals("Linebreak", StringComparison.OrdinalIgnoreCase))
+                seperator = DetectLinebreakType(toText);
+
+            string[] toTextParts = toText.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+
+            string joiner = JoinSelector.Text;
+            if (joiner.Equals("Linebreak", StringComparison.OrdinalIgnoreCase))
+                joiner = Environment.NewLine;
+
+            toText = string.Join(joiner, toTextParts);
+            ToTxtBox.Text = toText;
+        }
+
+        private static string DetectLinebreakType(string text)
+        {
+            int lineFeedCariageReturnCount = text.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).Length;
+            int lineFeedCount = text.Split("\n", StringSplitOptions.RemoveEmptyEntries).Length;
+            int cariageReturnCount = text.Split("\r", StringSplitOptions.RemoveEmptyEntries).Length;
+
+            if (lineFeedCariageReturnCount >= lineFeedCount && lineFeedCariageReturnCount >= cariageReturnCount)
+                return "\r\n";
+            else if (lineFeedCount > lineFeedCariageReturnCount && lineFeedCount > cariageReturnCount)
+                return "\n";
+            else
+                return "\r";
+        }
+    }
+}
